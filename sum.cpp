5f96532673ce94d4460819fa505dce6e3317e199
Sanskar Garg <sanskargarg_ee20a14_72@dtu.ac.in>
2021-12-08 23:35:07 +0530

    Initial Commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cin >> a >> b;
+
+    int sum=a+b;
+
+    cout << "Sum of " << a << " and " << b << " is => " << sum << endl;
+
+
+    return 0;
+}
